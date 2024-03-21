a6db49ba97c01abb1911b583c19f621d2edd1237
unknown <gptv88@gmail.com>
2024-03-21 15:44:07 -0700

    starter code

---
+++ src/componets/Navbar.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Logo from "../assets/Star-of-David.jpg";
+import { Link } from "react-router-dom";
+import ReorderIcon from '@mui/icons-material/Reorder';
+import "../styles/Navbar.css";
+
+
+function Navbar() {
+
+const [openLinks, setOpenLinks] = useState(false)
+
+const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+};
+  return (
+    <div className="navbar">
+        <div className="leftSide" id={openLinks ? "open" : "closed"}>
+        <img src={Logo} alt="Mayen HVAC Logo" />
+        <div className="hiddenLinks"></div>
+        <Link to="/"> Home </Link>
+        <Link to="/Services"> Services </Link>
+        <Link to="/about"> about </Link>
+        <Link to="/contact"> contact </Link>
+        </div>
+        <div className="rightSide"></div>
+        <Link to="/"> Home </Link>
+        <Link to="/Services"> Services </Link>
+        <Link to="/about"> about </Link>
+        <Link to="/contact"> contact </Link>
+        <button onClick={toggleNavbar}>
+        <ReorderIcon />
+        </button>
+    </div>
+  )
+}
+
+export default Navbar;
